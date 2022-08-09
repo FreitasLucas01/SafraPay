ee72aa0b66b6593d8d43ab23170323cdb5fb62fd
Lucas Freitas <lucasfreitasdesa02@gmail.com>
2022-08-09 20:00:46 -0300

    .

--- js/modules/drop-down.js
+++ js/modules/drop-down.js
@@ -1,16 +1,40 @@
 export default function initDropDown() {
-  const input = document.querySelectorAll("[data-input] p")
-  // const paragrafo = document.querySelectorAll("[data-input] p")
 
-  function handleClick(e) {
-    input.forEach((itens) => {
-      itens.classList.remove("ativo")
+  const dropdown = document.querySelectorAll("[data-input] p");
+  const label = document.querySelectorAll("[data-input] label");
+
+  function handleDropDown(e) {
+    if (e.type === "touchstart")
+      e.preventDefault()
+    const ativoClass = "ativo";
+    const classChecked = this.classList.contains(ativoClass);
+
+    dropdown.forEach((itens) => {
+      itens.classList.remove(ativoClass)
     })
-    if(e.target !== "ativo")
-    this.classList.toggle("ativo")
+    label.forEach((itens) => {
+      itens.classList.remove(ativoClass)
+    })
+    if (!classChecked) {
+      this.classList.toggle(ativoClass)
+      this.nextElementSibling.classList.toggle(ativoClass)
+    }
   }
 
-  input.forEach((itens) => {
-    itens.addEventListener("click", handleClick)
+  dropdown.forEach(itens => {
+    itens.addEventListener("click", handleDropDown)
+    itens.addEventListener("touchstart", handleDropDown)
+  })
+
+  function handleLabel(e) {
+    if (e.type === "touchstart")
+      e.preventDefault()
+    this.classList.remove("ativo")
+    this.previousElementSibling.classList.remove("ativo")
+  }
+
+  label.forEach(itens => {
+    itens.addEventListener("click", handleLabel)
+    itens.addEventListener("touchstart", handleLabel)
   })
 }
